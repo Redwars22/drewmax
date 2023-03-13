aebb27cec6c5bae6362f3f3a1bc97f596b86df6a
AndrePereira22h1 <andre@madatech.com.br>
2023-03-13 14:53:06 -0300

    adicionando hint a componentes de botão

--- src/components/Hero/HeroButtons/HeroButton.tsx
+++ src/components/Hero/HeroButtons/HeroButton.tsx
@@ -3,7 +3,11 @@ import styles from "../../../styles/elements.module.scss";
 
 export default function HeroButtonComponent(props: IHeroButton) {
   return (
-    <button onClick={props.action} className={styles["default-button"]}>
+    <button
+      onClick={props.action}
+      className={styles["default-button"]}
+      title={props.hint}
+    >
       <i className={`bi bi-${props.icon}`}></i>
       {props.title}
     </button>
